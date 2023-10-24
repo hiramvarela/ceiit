3d1e00432594813a1f171f47a3d0932cb6110bff
ITIT <ITIT@ULSAs-iMac.local>
2023-10-24 15:51:45 -0600

    Se agregan los CRUD de objectos

--- controllers/objects.controller.js
+++ controllers/objects.controller.js
@@ -9,21 +9,88 @@ async function addObject(req, res){
 
 	try {
 		const newObject = await new Object({
-			nombre:object,
-			numeroSerial: numberS,
-            ubicacio : ubica,
-            descripcio : descrip,
-            cantida : canti
+			name:object,
+			numserial: numberS,
+            ubicacion : ubica,
+            descripcion: descrip,
+            cantidad : canti
 		}).save();
 
 		res.json({
 			obj: newObject
+
+
 		})
 	} catch (err) {
 		console.log(err);
 	}
 }
+//PENDIENTE
+// async function updateObject(req,res){
+// 	const object = req.body.ob;
+// 	const numberS = req.body.num;
+//     const ubica = req.body.ubi;
+//     const descrip = req.body.des;
+//     const canti = req.body.cant;
+    
+// 	try{
+// 		const updateOb = await Object.updateOne({
+// 			name:object
+		
+// 		},{
+// 			$set : {
+				
+// 			}
+// 		});
+//         if(!updateOb){
+// 			res.status(401).json({mensaje: "No se encontro el objecto"})
+// 		}
+// 		res.json({
+// 			obj : updateOb
+// 		})
+// 	}catch(err){
+// 		console.log(err)
+// 		res.status(500).json({mensaje : "Hubo un error al buscar el objecto"})
+// 	}
+// }
+async function deleteObject(req,res){
+	const object = req.body.ob;
+	try{
+		const deleteO = await Object.deleteOne({
+			name:object
+		});
+        if(!deleteO){
+			res.status(401).json({mensaje: "No se encontro el objecto"})
+		}
+		res.json({
+			obj : deleteO
+		})
+	}catch(err){
+		console.log(err)
+		res.status(500).json({mensaje : "Hubo un error al borrar el objecto"})
+	}
+}
+async function readObject(req,res){
+	const object = req.body.ob;
+
+    
+	try{
+		const updateOb = await Object.findOne({
+			name:object
+		
+		});
+        if(!updateOb){
+			res.status(401).json({mensaje: "No se encontro el objecto"})
+		}
+		res.json({
+			obj : updateOb
+		})
+	}catch(err){
+		console.log(err)
+		res.status(500).json({mensaje : "Hubo un error al buscar el objecto"})
+	}
+}
 
 module.exports = {
-	addObject
+	addObject,readObject,deleteObject
 };
--- routes/ulsa.js
+++ routes/ulsa.js
@@ -2,11 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 const {
-    addObject
+    addObject,readObject,deleteObject
   } = require('../controllers/objects.controller.js');
+const  {
+    loanObject,loanDeleteObject,loanReadObject
+} = require('../controllers/loan.controller.js');
 
   router.post('/addObject', addObject);
-//   router.post('/iniciar_sesion', iniciarSesion);
+  router.post('/readObject', readObject);
+  router.post('/deleteObject', deleteObject);
+
+
+  router.post('/loanObject', loanObject);
+  router.post('/loanReadObject', loanReadObject);
+  router.post('/loanDeleteObject', loanDeleteObject);
+
   
   module.exports = router;
     
